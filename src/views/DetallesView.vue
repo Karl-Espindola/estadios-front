a5ce4b72eff26fc0728b801c3d04ee85106e0db4
Karl <karlespindola2022@gmail.com>
2022-09-07 11:09:38 -0500

    Vista Detalles

---
+++ src/views/DetallesView.vue
@@ -0,0 +1,35 @@
+<template>
+  
+    <div class="contenedor">
+      <p class="etiq">Estadios  ></p>
+      <InfoEstadio></InfoEstadio>
+      
+    </div>
+
+  </template>
+  
+  <script>
+  
+  import InfoEstadio from '@/components/InfoEstadio.vue';
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      InfoEstadio,
+      
+    }
+  }
+  </script>
+  <style scoped>
+  
+  
+  .contenedor{
+    padding:0 50px;
+    background-color: #F4F6F6;
+  }
+  .etiq{
+    padding: 15px 20px;
+    
+    color: #905AFB;
+  }
+  </style>
