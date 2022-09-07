5aa87b194db4e45383f576f4850571d7c97f26fd
Karl <karlespindola2022@gmail.com>
2022-09-07 11:30:09 -0500

    Vista Editar

---
+++ src/views/EditarView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="contenedor">
+        <p class="etiq">Estadios  > Editar</p>
+        <EditarEstadio></EditarEstadio>
+    </div>
+</template>
+
+<script>
+import EditarEstadio from '@/components/EditarEstadio.vue';
+    export default{
+        name:"EditarView",
+        components:{
+            EditarEstadio
+        }
+    }
+</script>
+<style >
+.contenedor{
+    padding-left:70px;
+}
+</style>
